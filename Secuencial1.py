f1ac860b4a5c07ab1761d64d28448b069dbda720
Only-Raiden <guillermooscar07@gmail.com>
2025-05-18 23:38:06 -0600

    Secuencial1.py

--- Secuencial1.py
+++ Secuencial1.py
@@ -1,10 +1,10 @@
-
 def busqueda_secuencial(lista, objetivo):
     for i in range(len(lista)):
         if lista[i] == objetivo:
             return i
     return -1
 
+# Lista desordenada
 numeros = [23, 12, 45, 67, 89, 10, 33]
 objetivo = 67
 
@@ -13,4 +13,3 @@ if resultado != -1:
     print(f"El número {objetivo} se encontró en el índice {resultado}")
 else:
     print("Número no encontrado")
-
